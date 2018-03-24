86a74a38f121bd3c1f92513b1a4e280c1c8a5f5f
markmurimi <murimimg180@gmail.com>
2018-03-24 15:05:24 +0300

    modified profile.ts file

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { GithubService } from '../github.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers:[GithubService]
 })
 export class ProfileComponent implements OnInit {
-
+  user:any=[];
+  repos:any=[];
+  username:string;
+  
   constructor() { }
 
   ngOnInit() {
