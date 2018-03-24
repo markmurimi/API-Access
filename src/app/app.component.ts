c0299d9eac555d229a50a77fe39b1c608a9f9f09
markmurimi <murimimg180@gmail.com>
2018-03-24 15:00:12 +0300

    modify ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {ProfileComponent} from './profile/profile.component';
+import {HttpClient} from '@angular/common/http';
+import { GithubService } from './github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[GithubService]
 })
 export class AppComponent {
   title = 'app';
