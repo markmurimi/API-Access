efa6b7b8f9403aabf58c0325da60f8690bca94be
markmurimi <murimimg180@gmail.com>
2018-03-25 15:09:36 +0300

    modified the ts file

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -5,17 +5,36 @@ import { GithubService } from '../github.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css'],
-  providers:[GithubService]
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
   user:any=[];
   repos:any=[];
   username:string;
-  
-  constructor() { }
-
+  constructor(private _githubService: GithubService) {
+    this._githubService.getUser().subscribe(user => {
+      console.log(user);
+      this.user = user;
+    })
+    this._githubService.getRepos().subscribe(repos => {
+      //console.log(user);
+      this.repos = repos;
+    })
+  }
   ngOnInit() {
   }
+  searchUser() {
+    this._githubService.updateUser(this.username);
+    this._githubService.getUser().subscribe(user => {
+      this.user = user;
+    })
+    //console.log('It works');
+    this._githubService.getRepos().subscribe(repos => {
+      //console.log(user);
+      this.repos = repos;
+    })
 
+  }
 }
+
+  
