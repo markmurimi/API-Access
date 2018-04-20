de3b856fb1f2dab68662ae48dd868b8d4bef7bec
mark <murimimg180@gmail.com>
2018-04-20 08:39:48 +0300

    deleted the stsrt.sh file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { RepositoriesComponent } from './repositories/repositories.component'
+
+const appRoutes: Routes = [
+  {path:'repositories', component: RepositoriesComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    RepositoriesComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
